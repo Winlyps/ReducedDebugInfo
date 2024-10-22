9d7621072f74344dcc2c5b26c1af7aa10fbc0027
Winlypse <nts3@o2.pl>
2024-10-22 23:25:46 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/reducedDebugInfo/ReducedDebugInfo.kt
@@ -0,0 +1,14 @@
+package winlyps.reducedDebugInfo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ReducedDebugInfo : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
